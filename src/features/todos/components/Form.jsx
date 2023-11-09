c8cc7e9fc5f938544fa8096b3d13d2d567be9ad1
cheolminJOO <cheolmin.joo@gmail.com>
2023-11-09 12:40:29 +0900

    update

---
+++ src/features/todos/components/Form.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "../../../redux/modules/todos.js";
+
+const Form = () => {
+  const dispatch = useDispatch();
+  const toDoArr = useSelector((state) => state.todos.toDoArr)
+  const [title,setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const onChangeContents = (event) => {
+    setBody(event.target.value)
+  }
+  
+
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (!title && !body) return;
+    
+    const newTodo = ({
+      id: toDoArr.length +1,
+      title,
+      body,
+      isDone: false,
+    });
+    dispatch(addTodo(newTodo))
+  
+  };
+
+  return (
+    <StAddForm onSubmit={onSubmitHandler}>
+      <StInputGroup>
+        <StFormLabel>제목</StFormLabel>
+        <StAddInput
+          type="text"
+          name="title"
+          value={title}
+          onChange={onChangeTitle}
+        />
+        <StFormLabel>내용</StFormLabel>
+        <StAddInput
+          type="text"
+          name="body"
+          value={body}
+          onChange={onChangeContents}
+        />
+      </StInputGroup>
+      <StAddButton>추가하기</StAddButton>
+    </StAddForm>
+  );
+};
+
+export default Form;
+
+const StInputGroup = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const StFormLabel = styled.label`
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const StAddForm = styled.form`
+  background-color: #eee;
+  border-radius: 12px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 30px;
+  gap: 20px;
+`;
+
+const StAddInput = styled.input`
+  height: 40px;
+  width: 240px;
+  border: none;
+  border-radius: 12px;
+  padding: 0 12px;
+`;
+
+const StAddButton = styled.button`
+  border: none;
+  height: 40px;
+  cursor: pointer;
+  border-radius: 10px;
+  background-color: teal;
+  width: 140px;
+  color: #fff;
+  font-weight: 700;
+`;
